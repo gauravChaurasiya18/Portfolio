20ba76caeabadd38d551abd2bf185dfa0122a0c8
gauravChaurasiya18 <gauravcha18@gmail.com>
2025-04-02 01:18:34 +0530

    Portfolio done

---
+++ src/data/constants.js
@@ -0,0 +1,237 @@
+export const Bio = {
+  name: "Gaurav Chaurasiya",
+  roles: ["Full Stack Developer", "Programmer"],
+  description:
+    "To kickstart my career as a software developer, utilizing my strong programming skills and eager to learn from experienced professionals. My goal is to contribute effectively to projects, innovate, and grow as a versatile developer while adding value to the organization",
+  github: "https://github.com/gauravChaurasiya18",
+  resume: "",
+  linkedin: "https://www.linkedin.com/in/gaurav-chaurasiya-822056266/",
+};
+
+export const skills = [
+  {
+    title: "Programming Languages",
+    skills: [
+      {
+        name: "C",
+        image: "C-logo.png",
+      },
+      {
+        name: "C++",
+        image: "CPP-logo.png",
+      },
+      {
+        name: "Java",
+        image: "java-logo.svg",
+      },
+      {
+        name: "Python",
+        image: "Python-logo.png",
+      },
+    ],
+  },
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "HTML",
+        image: "html5-logo.png",
+      },
+      {
+        name: "CSS",
+        image: "CSS3-logo.png",
+      },
+
+      {
+        name: "JavaScript",
+        image: "JavaScript-logo.png",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "Node Js",
+        image: "nodejs-logo.svg",
+      },
+      {
+        name: "Mongo DB",
+        image: "mongodb.png",
+      },
+      {
+        name: "Express Js",
+        image: "express.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image: "git.jpg",
+      },
+      {
+        name: "GitHub",
+        image: "GitHub-logo.png",
+      },
+      {
+        name: "VS Code",
+        image: "vscode-logo.png",
+      },
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "Ietlogo.png",
+    school: "Institute Of Engineering And Technology Lucknow ",
+    date: "2022 - 2026",
+    grade: "8.37 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering  at IET Institute Of Technology, Lucknow with a CGPA of 8.37. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others.",
+    degree:
+      "Bachelor of Technology - BTech, Computer Science and Engineering(AI)",
+  },
+  {
+    id: 1,
+    img: "gyandeep.png",
+    school: "Gyandeep English School  , Varanasi",
+    date: "2021 - 2022",
+    grade: " 90%",
+    desc: "I completed my Intermediate at Gyandeep English School  , Varanasi where I studied Maths, Physics and Chemistry stream.",
+    degree: "",
+  },
+  {
+    id: 2,
+    img: "gyandeep.png",
+    school: "Gyandeep English School  , Varanasi",
+    date: "2019-2020",
+    grade: "92.4% GPA",
+    desc: "I completed my class 10 education at Gyandeep English School  , Varanasi.",
+    degree: "",
+  },
+];
+
+export const coding = [
+  {
+    id: 10,
+    title: "Codforces",
+    description:
+      "I actively participate in Codeforces contests, constantly improving my problem-solving skills and algorithmic thinking. With a max rating of 1340 (Pupil),  I have tackled a wide range of problems",
+    image: "cf.png",
+    tags: ["Highest Rating 1340", " | Pupil"],
+    category: "web app",
+    github: "https://codeforces.com/profile/gau_r_av",
+    webapp: "https://codeforces.com/profile/gau_r_av",
+  },
+
+  {
+    id: 10,
+    title: "Codechef",
+    description:
+      "I am an enthusiastic problem solver and an active participant in CodeChef contests, consistently improving my algorithmic thinking and coding efficiency. Currently rated 3★, I have solved a variety of problems spanning.",
+    image: "cc-logo.svg",
+    tags: ["Highest Rating 1710", " | 3 Star"],
+    category: "",
+    github: "https://www.codechef.com/users/knight74",
+    webapp: "#",
+  },
+
+  {
+    id: 10,
+    title: "LeetCode",
+    description:
+      "I actively solve LeetCode problems to strengthen my Data Structures & Algorithms (DSA) knowledge and prepare for technical interviews. As a Knight, I have tackled problems across various difficulty levels.",
+    image: "leetcode.png",
+    tags: ["Highest Rating 1922", " | Knight"],
+    category: "web app",
+    github: "https://leetcode.com/u/Gaurav_chaurasiya/",
+  },
+
+  // {
+  //   id: 9,
+  //   title: "Simon-says",
+  //   description: `"Simon Says" is an exciting web project designed to keep users hooked on a memory challenge.`,
+  //   image: "simon-logo.png",
+  //   tags: ["HTML5", "CSS3", "JavaScript"],
+  //   category: "web app",
+  //   github: "https://github.com/kjcreddy666/simon",
+  //   webapp: "https://kjcreddy666.github.io/portfolio",
+  // },
+
+  // {
+  //   id: 9,
+  //   title: "Spotify-clone",
+  //   description:
+  //     "This project is a Spotify clone, aiming to replicate the user interface of the popular music streaming service using HTML and CSS.The primary goal of this project is to practice front-end development skil",
+  //   image: "spotify-logo.png",
+  //   tags: ["HTML5", "CSS3"],
+  //   category: "web app",
+  //   github: "https://github.com/kjcreddy666/spotify",
+  //   webapp: "https://kjcreddy666.github.io/spotify",
+  // },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "Algorithm Visualization",
+    description:
+      "Developed a custom Algorithm Visualization application using HTML5, CSS3, JavaScript, ReactJS, and the Google Custom Search API to provide fficient search results.",
+    image: "Google.png",
+    tags: ["HTML5", "CSS3", "JavaScript", "ReactJs"],
+    category: "web app",
+    github: "https://github.com/gauravChaurasiya18/Algorithm_visualizer",
+    webapp: "https://gauravchaurasiya18.github.io/Algorithm_visualizer/",
+  },
+
+  {
+    id: 9,
+    title: "Portfolio",
+    description:
+      "Explore my sleek portfolio, built with HTML5, CSS3, JavaScript, and React.js. Discover my skills and projects through seamless design and interactivity, optimized for any device.",
+    image: "portfolio-logo.png",
+    tags: ["HTML5", "CSS3", "JavaScript", "React"],
+    category: "web app",
+    github: "",
+    webapp: "#",
+  },
+
+  // {
+  //   id: 9,
+  //   title: "Tic Tac Toe",
+  //   description:
+  //     "Implemented a classic Tic Tac Toe game using HTML5, CSS3, JavaScript and ReactJS.",
+  //   image: "tictactoe-logo.png",
+  //   tags: ["HTML5", "CSS3", "JavaScript", "ReactJs"],
+  //   category: "web app",
+  //   github: "https://github.com/kjcreddy666/tictactoe",
+  //   webapp: "https://kjcreddy666.github.io/tictactoe",
+  // },
+
+  // {
+  //   id: 9,
+  //   title: "Simon-says",
+  //   description: `"Simon Says" is an exciting web project designed to keep users hooked on a memory challenge.`,
+  //   image: "simon-logo.png",
+  //   tags: ["HTML5", "CSS3", "JavaScript"],
+  //   category: "web app",
+  //   github: "https://github.com/kjcreddy666/simon",
+  //   webapp: "https://kjcreddy666.github.io/portfolio",
+  // },
+
+  // {
+  //   id: 9,
+  //   title: "Spotify-clone",
+  //   description:
+  //     "This project is a Spotify clone, aiming to replicate the user interface of the popular music streaming service using HTML and CSS.The primary goal of this project is to practice front-end development skil",
+  //   image: "spotify-logo.png",
+  //   tags: ["HTML5", "CSS3"],
+  //   category: "web app",
+  //   github: "https://github.com/kjcreddy666/spotify",
+  //   webapp: "https://kjcreddy666.github.io/spotify",
+  // },
+];
